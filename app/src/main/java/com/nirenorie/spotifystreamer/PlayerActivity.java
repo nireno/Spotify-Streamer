50abbc80e1bc51027fb0616c105ba91d73e1ca5c
Niren Orie <nireno@gmail.com>
2015-08-23 18:17:48 -0400

    fix: Up navigation issues
    Up navigation used to recreate the previous activity. In particular up from the PlayerActivity would cause the app to crash.

--- app/src/main/java/com/nirenorie/spotifystreamer/PlayerActivity.java
+++ app/src/main/java/com/nirenorie/spotifystreamer/PlayerActivity.java
@@ -3,6 +3,7 @@ package com.nirenorie.spotifystreamer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class PlayerActivity extends AppCompatActivity {
     public static final String EXTRA_ARTIST_ID = "ARTIST";
@@ -37,18 +38,21 @@ public class PlayerActivity extends AppCompatActivity {
 //        return true;
 //    }
 //
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+        return true;
+    } else if (id == android.R.id.home) {
+        onBackPressed();
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+}
 }
